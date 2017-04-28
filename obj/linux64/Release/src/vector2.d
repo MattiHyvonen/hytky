c30bb0c6464e06782483acd925ced1b4617452d8
Matti Hyvönen <mattihyvonen@gmail.com>
2017-04-28 10:03:58 +0300

    turhat pois

--- obj/linux64/Release/src/vector2.d
+++
@@ -1,5 +0,0 @@
-obj/linux64/Release/src/vector2.o: \
- /home/olento/koodaus/of_v0.9.8_linux64_release/apps/myApps/hytky/src/vector2.cpp \
- /home/olento/koodaus/of_v0.9.8_linux64_release/apps/myApps/hytky/src/vector2.h
-
-/home/olento/koodaus/of_v0.9.8_linux64_release/apps/myApps/hytky/src/vector2.h:
